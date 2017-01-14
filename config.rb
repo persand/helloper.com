2743e6ff2bc4347eabad8f61893b8766b7a434ac
Per Sandström <per@helloper.com>
2017-01-14 19:32:04 +0100

    Update Middleman config

--- config.rb
+++ config.rb
@@ -1,17 +1,23 @@
-activate :sprockets
-activate :autoprefixer
-activate :directory_indexes
-activate :syntax, :line_numbers => true
-
-# Configure path for assets
+# Configure paths for assets
 config[:css_dir] = 'assets/stylesheets'
 config[:js_dir] = 'assets/javascripts'
 config[:images_dir] = 'assets/images'
-config[:partials_dir] = 'partials'
+config[:images_extensions] = %w( svg jpg jpeg gif png webp )
 
+# Pull in assets installed from Bower
 @bower_config = JSON.parse(IO.read("#{root}/.bowerrc"))
-assets_dir = @bower_config["directory"] # "vendor/assets/bower_components"
-sprockets.append_path File.join app.root, assets_dir
+bower_assets_dir = @bower_config["directory"]
+
+activate :sprockets
+sprockets.append_path File.join app.root, bower_assets_dir
+
+masks = config[:images_extensions].map { |ext| "#{bower_assets_dir}/**/*.#{ext}" }
+
+# Import all the images from bower assets directory to build/assets/images
+Dir[*masks].each do |file_path|
+  relative_path = file_path[("#{bower_assets_dir}/".length)..-1] # e.g.: "open-iconic/png/resize-width.png"
+  import_file(file_path, "assets/images/#{relative_path}")
+end
 
 # Blog
 activate :blog do |blog|
@@ -24,6 +30,9 @@ end
 
 page "/feed.xml", :layout => false
 
+activate :directory_indexes
+activate :syntax, :line_numbers => true
+
 set :markdown, fenced_code_blocks: true, :smartypants => true
 set :markdown_engine, :redcarpet
 
@@ -32,13 +41,12 @@ configure :development do
   set :debug_assets, true
 end
 
-# Minimize css/js and fix assets for Build
 configure :build do
+  activate :autoprefixer
   activate :gzip
-  activate :minify_html
   activate :minify_css
+  activate :minify_html
   activate :minify_javascript
-  activate :asset_hash, :ignore => [/^media*/]
   activate :relative_assets
-  #activate :sitemap, :hostname => "https://www.helloper.com"
+  activate :asset_hash, :ignore => [/^media*/]
 end
