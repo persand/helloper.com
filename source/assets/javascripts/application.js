abe904c3ce3f8e49231abbb55693ec812d752f4b
Per Sandström <per@helloper.com>
2017-03-11 11:36:52 +0100

    Slideout WIP

--- source/assets/javascripts/application.js
+++ source/assets/javascripts/application.js
@@ -17,11 +17,6 @@ var slideout = new Slideout({
   'tolerance': 70
 });
 
-function close(eve) {
-  eve.preventDefault();
-  slideout.close();
-}
-
 html.classList.add('slideout-root');
 
 slideout
@@ -32,7 +27,6 @@ slideout
     html.classList.remove('slideout-root-overlay');
   });
 
-
 // --
 // Hamburger button click behaviour
 // --
@@ -41,31 +35,6 @@ var hamburger = document.querySelector(".hamburger");
 hamburger.addEventListener("click", function() {
   hamburger.classList.toggle("is-active");
   hamburger.classList.toggle("is-inactive");
-  hamburger.classList.add("is-used");
 
   slideout.toggle();
 });
-
-// --
-// Hamburger button scroll behaviour
-// --
-var navigationPreviousPosition = window.pageYOffset || document.documentElement.scrollTop;
-window.onscroll = function() {
-  var navigationCurrentPosition = window.pageYOffset || document.documentElement.scrollTop;
-
-  if (navigationPreviousPosition > navigationCurrentPosition) {
-    // Up
-    hamburger.classList.remove("is-hidden");
-    hamburger.classList.add("is-scrolling-up");
-
-    if (window.pageYOffset < 60) {
-      hamburger.classList.remove("is-scrolling-up");
-    }
-  } else {
-    // Down
-    hamburger.classList.add("is-hidden");
-    hamburger.classList.remove("is-scrolling-up");
-  }
-
-  navigationPreviousPosition = navigationCurrentPosition;
-};
