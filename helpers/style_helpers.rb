9cc4de49969a43e9f92e94471ca2aad120faf459
Per Sandström <per@helloper.com>
2017-03-12 12:46:35 +0100

    Misc

--- helpers/style_helpers.rb
+++ helpers/style_helpers.rb
@@ -14,4 +14,14 @@ module StyleHelpers
       " class=\"#{classes.to_s}\""
     end
   end
+
+  # http://stackoverflow.com/questions/2129928/rails-split-and-wrap-each-character-in-a-string
+  #
+  # <%- @text = "abc" -%>
+  # <%- wrap_each_char(@text) do |c| -%>
+  # <span><%= c %></span>
+  # <%- end -%>
+  def wrap_each_char(string, &block)
+    string.each_char.map(&block).join
+  end
 end
